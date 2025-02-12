99f6193d4ccc9dbd74d934a8c9e3b871dd95384b
yoggsoft <yoggsoft@gmail.com>
2025-02-12 16:59:35 +0100

    remove privacy page from robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots (): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/css', 'privacy']
+        disallow: ['/css']
       }
     ],
     sitemap: 'https://manuelreyes.dev/sitemap.xml'

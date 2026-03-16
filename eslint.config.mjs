8d7b3a67897df7a5e239182394eb9d69988e12e2
Manuel <yoggsoft@gmail.com>
2026-03-16 12:40:46 +0100

    chore:update next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,18 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      quotes: ['error', 'single'],
-      indent: ['error', 2]
-    }
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    quotes: ['error', 'single'],
+    indent: ['error', 2]
   }
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
